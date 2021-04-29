b7bedc3206476bec775c36217c78ec79371b702c
Jmaryett <wtfmahariel@gmail.com>
2021-04-29 18:49:44 +0300

    a

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -2,16 +2,29 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *copy; size_t i = 0; size_t j = ft_strlen(s1); size_t k = ft_strlen(set); size_t l = 0;
+	char *copy; size_t i = 0; size_t j = 0;
 
 	if (!s1 || !set)
 		return (NULL);
 	copy = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!copy)
 		return (NULL);
-	while (s1[i] == set[i])
+	while (s1[i])
+	{
+		if (s1[i] == set[i])
+			i++;
+		copy[j] = s1[i];
+		j++;
 		i++;
-	
-	copy[l] = '\0';
+	}
+	copy[j] = '\0';
 	return (copy);
+}
+
+int main()
+{
+	char s1[] = "\t \n  \n12345dfdd\t  \n";
+	char set[] = "\t\n";
+	
+	printf("%s", ft_strtrim(s1, set));
 }
