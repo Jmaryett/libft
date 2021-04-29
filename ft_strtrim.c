587b7d041d5bfb9cd0d98f8d1f6d52a8126c17eb
Jmaryett <wtfmahariel@gmail.com>
2021-04-29 19:29:28 +0300

    a

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -2,29 +2,28 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	char *copy; size_t i = 0; size_t j = 0;
-
+	char *copy; size_t i = 0; size_t j = 0; size_t k = ft_strlen(s1); size_t l = 0;
 	if (!s1 || !set)
 		return (NULL);
 	copy = (char*)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!copy)
 		return (NULL);
-	while (s1[i])
-	{
-		if (s1[i] == set[i])
-			i++;
-		copy[j] = s1[i];
-		j++;
+	while (s1[i] >= 9 && s1[i] <= 13 || s1[i] == ' ')
 		i++;
-	}
+	while (s1[k] >= 9 && s1[k] <= 13 || s1[k] == ' ')
+		k--;
+	while (s1[i] != s1[k])
+		copy[j++] = s1[i++];
 	copy[j] = '\0';
 	return (copy);
 }
 
-int main()
+/* int main()
 {
-	char s1[] = "\t \n  \n12345dfdd\t  \n";
-	char set[] = "\t\n";
-	
-	printf("%s", ft_strtrim(s1, set));
-}
+	char *s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
+        char *s2 = "Hello \t  Please\n Trim me !";
+        char *ret = ft_strtrim(s1, " \n\t");
+
+	printf ("%s\n", s2);
+	printf ("%s", ret);
+} */
