e5b8a127ef7d014231315a9dc1ca776a4a14d2d7
Jmaryett <wtfmahariel@gmail.com>
2021-05-03 21:17:21 +0300

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -2,27 +2,24 @@
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	unsigned int s_len = ft_strlen(s); size_t i = 0;
-	char *sub;
-	if (!s)
-		return (NULL);
-	if (s_len < start)
-	{
+    unsigned int i;
+    char *sub;
+
+    if(!s)
+        return (NULL);
+    i = 0;
+    if (ft_strlen(s) < start)
+    {
 		sub = (char*)malloc(sizeof(char));
 		sub[i] = '\0';
 		return (sub);
 	}
-	if (s_len < len)
-		len = s_len;
-	sub = (char*)malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
-	while (i < len)
-	{
-		sub[i] = s[start];
-		i++; 
-		start++;
-	}
-	sub[i] = '\0';
-	return (sub);
+    if (ft_strlen(s) < len)
+        len = ft_strlen(s);
+    if (!(sub = (char*)malloc(sizeof(char) * (len + 1))))
+        return (NULL);
+    while (i < len && s[start])
+        sub[i++] = s[start++];
+    sub[i] = '\0';
+    return (sub);
 }
