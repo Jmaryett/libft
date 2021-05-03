7a69f78d282c7a1f5625f7f9350fbca28d7c36fc
Jmaryett <wtfmahariel@gmail.com>
2021-05-03 20:11:14 +0300

    enddd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -4,9 +4,13 @@ void ft_putnbr_fd(int n, int fd)
 {
     char c;
 
+    if (fd == -1)
+        return (0);
+    if (n == -2147483647 -1)
+        write(fd, "-2147483648", 11);
     if (n < 0)
     {
-        ft_putchar_fd("-", fd);
+        ft_putchar_fd('-', fd);
         ft_putnbr_fd(-n, fd);
     }
     if (n > 9)
