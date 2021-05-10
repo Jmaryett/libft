9dee20f88ad53cacd59b98e8d265359b44f3ff7a
Stepan Kirillov <chudapak@Stepans-MacBook-Pro.local>
2021-05-10 18:22:16 +0300

    all gooddddddddddddd

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -8,18 +8,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if(!lst || !f)
 		return (NULL);
-	elem = NULL;
+	new = NULL;
 	while(lst)
 	{
 		tmp = lst->content;
 		elem = ft_lstnew((*f)(tmp));
 		if (!elem)
 		{
-			del(elem);
-			free(elem);
+			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		ft_lstadd_back(new, elem);
+		ft_lstadd_back(&new, elem);
 		lst = lst->next;
 	}
 	return (new);
