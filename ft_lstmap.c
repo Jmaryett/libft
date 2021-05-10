39d493db956a017b4e1f5626bf6854bb39afdfa4
Stepan Kirillov <chudapak@Stepans-MacBook-Pro.local>
2021-05-10 17:48:51 +0300

    d

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -12,10 +12,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		tmp = lst->content;
 		new = (t_list*)malloc(sizeof(t_list));
 		if (!new)
-			return NULL;
+			del(new);
 		new->content = f(tmp);
 		lst = lst->next;
-		del(new);
 	}
 	return (NULL);
 }
